9cb148276293316718ee58d4381ab96de2c685cc
Virtute90 <32056724+Virtute90@users.noreply.github.com>
2024-03-04 22:04:15 +0000

    Aggiunto thema

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,17 +1,40 @@
+import { Preview } from "@storybook/react";
 import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
+import React, { CSSProperties } from "react";
 import "../assets/css/fonts.css";
 import "../assets/css/storybook-fixes.css";
-import { Preview } from "@storybook/react";
+import { FontLoader } from "../src";
+import theme from "./theme";
+
+const customStyle = {
+    position: 'relative',
+    boxSizing: 'border-box',
+    margin: '0',
+    padding: '48px 32px',
+    textAlign: 'initial'
+} as CSSProperties;
+
 
 const preview: Preview = {
     parameters: {
         actions: { argTypesRegex: "^on[A-Z].*" },
+        docs: {
+            theme: theme
+        },
+        decorators: [
+            (Story) => (
+                <div style={customStyle}>
+                    <FontLoader />
+                    <Story />
+                </div>
+            ),
+        ],
         options: {
             storySort: {
                 order: [
                     "Documentazione",
                     [
-                        "welcome-page",
+                        "form",
                         "organizzare",
                         "menu",
                         "componenti",
