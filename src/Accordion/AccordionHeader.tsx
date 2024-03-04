3e6fbaa25d30275091212ecc8338623f61aa96de
Virtute90 <32056724+Virtute90@users.noreply.github.com>
2024-03-04 17:26:47 +0000

    Aggiunte interfaccie

--- src/Accordion/AccordionHeader.tsx
+++ src/Accordion/AccordionHeader.tsx
@@ -1,44 +1,36 @@
-import React, { FC, ReactNode, ElementType } from 'react';
-import classNames from 'classnames';
+import React, { FC, ReactNode, ElementType, HTMLAttributes } from "react";
+import classNames from "classnames";
 
-export interface AccordionHeaderProps {
-  /** Utilizzarlo in caso di utilizzo di componenti personalizzati */
-  tag?: ElementType;
-  /** Classi aggiuntive da usare per il componente AccordionHeader */
-  className?: string;
-  /** Utilizzare questo attributo per indicare se l'elemento è attivo o no */
-  active?: boolean;
-  /** Questa funzione verrà chiamata quando avviene un click sul componente AccordionHeader */
-  onToggle?: () => void;
-  /** Contenuto aggiuntivo all'interno del bottone dell'AccordionHeader */
-  append?: ReactNode;
-  testId?: string;
+export interface AccordionHeaderProps extends HTMLAttributes<HTMLElement> {
+    /** Utilizzarlo in caso di utilizzo di componenti personalizzati */
+    tag?: ElementType;
+    /** Classi aggiuntive da usare per il componente AccordionHeader */
+    className?: string;
+    /** Utilizzare questo attributo per indicare se l'elemento è attivo o no */
+    active?: boolean;
+    /** Questa funzione verrà chiamata quando avviene un click sul componente AccordionHeader */
+    onToggle?: () => void;
+    /** Contenuto aggiuntivo all'interno del bottone dell'AccordionHeader */
+    append?: ReactNode;
+    testId?: string;
 }
 
-export const AccordionHeader: FC<AccordionHeaderProps> = ({
-  className,
-  tag = 'button',
-  active = false,
-  append,
-  onToggle,
-  testId,
-  ...attributes
-}) => {
-  const Tag = tag;
-  const toggleClasses = classNames(className, 'accordion-button', {
-    collapsed: !active
-  });
-  return (
-    <div className='accordion-header' data-testid={testId}>
-      <Tag
-        data-bs-toggle='collapse'
-        aria-expanded={active ? 'true' : 'false'}
-        className={toggleClasses}
-        onClick={onToggle}
-        {...attributes}
-        {...(tag === 'button' && { type: 'button' })}
-      ></Tag>
-      {append}
-    </div>
-  );
+export const AccordionHeader: FC<AccordionHeaderProps> = ({ className, tag = "button", active = false, append, onToggle, testId, ...attributes }) => {
+    const Tag = tag;
+    const toggleClasses = classNames(className, "accordion-button", {
+        collapsed: !active,
+    });
+    return (
+        <div className="accordion-header" data-testid={testId}>
+            <Tag
+                data-bs-toggle="collapse"
+                aria-expanded={active ? "true" : "false"}
+                className={toggleClasses}
+                onClick={onToggle}
+                {...attributes}
+                {...(tag === "button" && { type: "button" })}
+            ></Tag>
+            {append}
+        </div>
+    );
 };
--- src/Accordion/AccordionItem.tsx
+++ src/Accordion/AccordionItem.tsx
@@ -1,16 +1,16 @@
-import React, { ElementType, FC } from 'react';
-import classNames from 'classnames';
+import React, { ElementType, FC, HTMLAttributes } from "react";
+import classNames from "classnames";
 
-export interface AccordionItemProps {
-  /** Utilizzarlo in caso di utilizzo di componenti personalizzati */
-  tag?: ElementType;
-  /** Classi aggiuntive da usare per il componente */
-  className?: string;
+export interface AccordionItemProps extends HTMLAttributes<HTMLElement> {
+    /** Utilizzarlo in caso di utilizzo di componenti personalizzati */
+    tag?: ElementType;
+    /** Classi aggiuntive da usare per il componente */
+    className?: string;
 }
 
-export const AccordionItem: FC<AccordionItemProps> = ({ tag = 'div', className, ...attributes }) => {
-  const Tag = tag;
-  const classes = classNames('accordion-item', className);
+export const AccordionItem: FC<AccordionItemProps> = ({ tag = "div", className, ...attributes }) => {
+    const Tag = tag;
+    const classes = classNames("accordion-item", className);
 
-  return <Tag {...attributes} className={classes} />;
+    return <Tag {...attributes} className={classes} />;
 };
