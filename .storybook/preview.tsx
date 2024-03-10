1ad656bd3a448a1b9ef409fb5f911701c6f95d81
Virtute90 <32056724+Virtute90@users.noreply.github.com>
2024-03-10 14:55:46 +0000

    Ordine pagine funziona!

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,49 +1,30 @@
 import { Preview } from "@storybook/react";
 import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
-import React, { CSSProperties } from "react";
+import { CSSProperties } from "react";
 import "../assets/css/fonts.css";
 import "../assets/css/storybook-fixes.css";
-import { FontLoader } from "../src";
 import theme from "./theme";
 
 const customStyle = {
-    position: 'relative',
-    boxSizing: 'border-box',
-    margin: '0',
-    padding: '48px 32px',
-    textAlign: 'initial'
+    position: "relative",
+    boxSizing: "border-box",
+    margin: "0",
+    padding: "48px 32px",
+    textAlign: "initial",
 } as CSSProperties;
-
 
 const preview: Preview = {
     parameters: {
-        actions: { argTypesRegex: "^on[A-Z].*" },
+        actions: { argTypesRegex: "^on.*" },
         docs: {
-            theme: theme
+            theme: theme,
+            toc: true,
         },
-        decorators: [
-            (Story) => (
-                <div style={customStyle}>
-                    <FontLoader />
-                    <Story />
-                </div>
-            ),
-        ],
         options: {
             storySort: {
                 order: [
                     "Documentazione",
-                    [
-                        "form",
-                        "organizzare",
-                        "menu",
-                        "componenti",
-                        "form",
-                        "utilities",
-                        "domande-frequenti-page",
-                        "changelog-page",
-                        "migrazione-da-v1-page",
-                    ],
+                    ["Welcome", "Organizzare gli spazi", "Organizzare i contenuti", "Menu di navigazione", "Componenti", "Form", "Utilities"],
                     "*",
                 ],
             },
