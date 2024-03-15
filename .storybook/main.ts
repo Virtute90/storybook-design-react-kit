8b01341c195cc4e712aeba98b7928abba8c90bdd
Virtute90 <32056724+Virtute90@users.noreply.github.com>
2024-03-15 14:36:53 +0000

    Rimosso pacchetto

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
     stories: ["../stories/**/*.@(mdx|stories.@(ts|tsx|js|jsx))", "../stories/**/*.mdx"],
-    addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-onboarding", "@storybook/addon-interactions", "@storybook/addon-a11y"],
+    addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y"],
     framework: {
         name: "@storybook/react-vite",
         options: {},
