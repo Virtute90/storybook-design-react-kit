b7fc65998c2037d3b188212ce1e49cd358482295
Virtute90 <matteo.ave@gmail.com>
2024-03-21 17:31:01 +0100

    Doc e storia breadcrumb ok

--- stories/Components/Breadcrumb.stories.tsx
+++ stories/Components/Breadcrumb.stories.tsx
@@ -1,94 +1,99 @@
-/* eslint jsx-a11y/anchor-is-valid: 0 */
+import { Meta, StoryObj } from "@storybook/react";
 import React from "react";
-
 import { Breadcrumb, BreadcrumbItem, Icon } from "../../src";
 
-export default {
-    title: "Componenti/Breadcrumbs",
+const meta: Meta<typeof Breadcrumb> = {
+    title: "Documentazione/Menu di navigazione/Breadcrumbs",
+    component: Breadcrumb,
 };
 
-export const Esempi = () => (
-    <section>
-        <Breadcrumb>
-            <BreadcrumbItem>
-                <a href="#">Home</a>
-                <span className="separator">/</span>
-            </BreadcrumbItem>
-            <BreadcrumbItem>
-                <a href="#">Subsection</a>
-                <span className="separator">/</span>
-            </BreadcrumbItem>
-            <BreadcrumbItem active>Current section</BreadcrumbItem>
-        </Breadcrumb>
+export default meta;
+type Story = StoryObj<typeof Breadcrumb>;
 
-        <Breadcrumb>
-            <BreadcrumbItem>
-                <a href="#">Home</a>
-                <span className="separator">&gt;</span>
-            </BreadcrumbItem>
-            <BreadcrumbItem>
-                <a href="#">Subsection</a>
-                <span className="separator">&gt;</span>
-            </BreadcrumbItem>
-            <BreadcrumbItem active>Current section</BreadcrumbItem>
-        </Breadcrumb>
-    </section>
-);
+export const Esempi: Story = {
+    render: () => (
+        <section>
+            <Breadcrumb>
+                <BreadcrumbItem>
+                    <a href="#">Home</a>
+                    <span className="separator">/</span>
+                </BreadcrumbItem>
+                <BreadcrumbItem>
+                    <a href="#">Subsection</a>
+                    <span className="separator">/</span>
+                </BreadcrumbItem>
+                <BreadcrumbItem active>Current section</BreadcrumbItem>
+            </Breadcrumb>
 
-export const ConIcone = () => (
-    <section>
-        <Breadcrumb>
-            <BreadcrumbItem>
-                <Icon className="align-top me-1" icon="it-link" color="secondary" aria-hidden size="sm" />
-                <a href="#">Home</a>
-                <span className="separator">/</span>
-            </BreadcrumbItem>
-            <BreadcrumbItem>
-                <Icon className="align-top me-1" icon="it-link" color="secondary" aria-hidden size="sm" />
-                <a href="#">Subsection</a>
-                <span className="separator">/</span>
-            </BreadcrumbItem>
-            <BreadcrumbItem active>
-                <Icon className="align-top me-1" icon="it-link" color="secondary" aria-hidden size="sm" />
-                Current section
-            </BreadcrumbItem>
-        </Breadcrumb>
-    </section>
-);
+            <Breadcrumb>
+                <BreadcrumbItem>
+                    <a href="#">Home</a>
+                    <span className="separator">&gt;</span>
+                </BreadcrumbItem>
+                <BreadcrumbItem>
+                    <a href="#">Subsection</a>
+                    <span className="separator">&gt;</span>
+                </BreadcrumbItem>
+                <BreadcrumbItem active>Current section</BreadcrumbItem>
+            </Breadcrumb>
+        </section>
+    ),
+};
 
-ConIcone.storyName = "Con icone";
+export const ConIcone: Story = {
+    render: () => (
+        <section>
+            <Breadcrumb>
+                <BreadcrumbItem>
+                    <Icon className="align-top me-1" icon="it-link" color="secondary" aria-hidden size="sm" />
+                    <a href="#">Home</a>
+                    <span className="separator">/</span>
+                </BreadcrumbItem>
+                <BreadcrumbItem>
+                    <Icon className="align-top me-1" icon="it-link" color="secondary" aria-hidden size="sm" />
+                    <a href="#">Subsection</a>
+                    <span className="separator">/</span>
+                </BreadcrumbItem>
+                <BreadcrumbItem active>
+                    <Icon className="align-top me-1" icon="it-link" color="secondary" aria-hidden size="sm" />
+                    Current section
+                </BreadcrumbItem>
+            </Breadcrumb>
+        </section>
+    ),
+};
 
-export const SuSfondoScuro = () => (
-    <section>
-        <Breadcrumb dark>
-            <BreadcrumbItem>
-                <a href="#">Home</a>
-                <span className="separator">/</span>
-            </BreadcrumbItem>
-            <BreadcrumbItem>
-                <a href="#">Subsection</a>
-                <span className="separator">/</span>
-            </BreadcrumbItem>
-            <BreadcrumbItem active>Current section</BreadcrumbItem>
-        </Breadcrumb>
+export const SuSfondoScuro: Story = {
+    render: () => (
+        <section>
+            <Breadcrumb dark>
+                <BreadcrumbItem>
+                    <a href="#">Home</a>
+                    <span className="separator">/</span>
+                </BreadcrumbItem>
+                <BreadcrumbItem>
+                    <a href="#">Subsection</a>
+                    <span className="separator">/</span>
+                </BreadcrumbItem>
+                <BreadcrumbItem active>Current section</BreadcrumbItem>
+            </Breadcrumb>
 
-        <Breadcrumb dark>
-            <BreadcrumbItem>
-                <Icon className="align-top me-1" icon="it-link" color="white" aria-hidden size="sm" />
-                <a href="#">Home</a>
-                <span className="separator">/</span>
-            </BreadcrumbItem>
-            <BreadcrumbItem>
-                <Icon className="align-top me-1" icon="it-link" color="white" aria-hidden size="sm" />
-                <a href="#">Subsection</a>
-                <span className="separator">/</span>
-            </BreadcrumbItem>
-            <BreadcrumbItem active>
-                <Icon className="align-top me-1" icon="it-link" color="white" aria-hidden size="sm" />
-                Current section
-            </BreadcrumbItem>
-        </Breadcrumb>
-    </section>
-);
-
-SuSfondoScuro.storyName = "Su sfondo scuro";
+            <Breadcrumb dark>
+                <BreadcrumbItem>
+                    <Icon className="align-top me-1" icon="it-link" color="white" aria-hidden size="sm" />
+                    <a href="#">Home</a>
+                    <span className="separator">/</span>
+                </BreadcrumbItem>
+                <BreadcrumbItem>
+                    <Icon className="align-top me-1" icon="it-link" color="white" aria-hidden size="sm" />
+                    <a href="#">Subsection</a>
+                    <span className="separator">/</span>
+                </BreadcrumbItem>
+                <BreadcrumbItem active>
+                    <Icon className="align-top me-1" icon="it-link" color="white" aria-hidden size="sm" />
+                    Current section
+                </BreadcrumbItem>
+            </Breadcrumb>
+        </section>
+    ),
+};
