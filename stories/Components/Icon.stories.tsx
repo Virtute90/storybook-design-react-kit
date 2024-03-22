cd29bfa942592815ab4bbf7beb7aac34618f2096
Virtute90 <matteo.ave@gmail.com>
2024-03-22 23:20:04 +0000

    Doc e storia Icon ok

--- stories/Components/Icon.stories.tsx
+++ stories/Components/Icon.stories.tsx
@@ -1,56 +1,43 @@
-import { Story } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
+import { Col, Icon, Row, icons } from '../../src';
 
-import { Icon, Row, Col, icons } from '../../src';
-
-export default {
-  title: 'Componenti/Icon'
+const meta: Meta<typeof Icon> = {
+  title: "Documentazione/Utilities/Icon",
+  component: Icon,
 };
 
-type EsempioInterattivoProps = {
-  icon: string;
-  title: string;
-  size: '' | 'xl' | 'lg' | 'sm' | 'xs';
-  padding: boolean;
-  color: string;
-  background: string;
-};
+export default meta;
 
-export const EsempioInterattivo: Story<EsempioInterattivoProps> = ({
-  icon,
-  title,
-  size,
-  padding,
-  background,
-  color
-}) => {
-  return <Icon icon={icon} size={size} title={title} padding={padding} color={color} className={background} />;
-};
+type Story = StoryObj<typeof Icon>;
 
-EsempioInterattivo.storyName = 'Esempio interattivo';
-EsempioInterattivo.args = {
-  icon: 'it-tool',
-  title: 'Alt Text',
-  size: '',
-  padding: false,
-  color: '',
-  background: ''
-};
-EsempioInterattivo.argTypes = {
-  icon: {
-    control: {
-      type: 'select',
+export const EsempioInterattivo: Story = {
+  render: ({
+    icon,
+    title,
+    size,
+    padding,
+    className,
+    color
+  }) => (<Icon icon={icon} size={size} title={title} padding={padding} color={color} className={className} />),
+  args: {
+    icon: 'it-tool',
+    title: 'Alt Text',
+    size: '',
+    padding: false,
+    color: '',
+    className: ''
+  },
+  argTypes: {
+    icon: {
+      control: 'select',
       options: icons
-    }
-  },
-  title: {
-    control: {
-      type: 'text'
-    }
-  },
-  size: {
-    control: {
-      type: 'select',
+    },
+    title: {
+      control: 'text'
+    },
+    size: {
+      control: 'select',
       options: {
         'Extra small': 'xs',
         Small: 'sm',
@@ -58,106 +45,107 @@ EsempioInterattivo.argTypes = {
         Large: 'lg',
         'Extra Large': 'xl'
       }
-    }
-  },
-  background: {
-    control: {
-      type: 'select',
+    },
+    className: {
+      control: 'select',
       options: {
         default: '',
         Scuro: 'bg-dark',
         Chiaro: 'bg-light'
       }
-    }
-  },
-  color: {
-    control: {
-      type: 'select',
+    },
+    color: {
+      control: 'select',
       options: ['primary', 'secondary', 'success', 'warning', 'danger', 'light', 'white']
     }
   }
 };
 
-export const EsempioDimensioni: Story<EsempioInterattivoProps> = ({ icon, padding, background, color }) => {
-  const dimensions = ['xs', 'sm', '', 'lg', 'xl'] as const;
-  return (
-    <>
-      {dimensions.map((size) => (
-        <Icon key={size} icon={icon} size={size} padding={padding} color={color} className={background} />
-      ))}
-    </>
-  );
+export const EsempioDimensioni: Story = {
+  render: ({ icon, padding, className, color }) => {
+    const dimensions = ['xs', 'sm', '', 'lg', 'xl'] as const;
+    return (
+      <>
+        {dimensions.map((size) => (
+          <Icon key={size} icon={icon} size={size} padding={padding} color={color} className={className} />
+        ))}
+      </>
+    );
+  },
+  args: {
+    icon: 'it-check-circle',
+    padding: false,
+    color: '',
+    className: 'bg-grey'
+  }
 };
 
-EsempioDimensioni.storyName = 'Esempio dimensioni';
-EsempioDimensioni.args = {
-  icon: 'it-check-circle',
-  padding: false,
-  color: '',
-  background: 'bg-grey'
-};
+export const EsempioPadding: Story = {
+  args: {
+    ...EsempioDimensioni.args,
+    icon: 'it-check-circle',
+    padding: true,
+    color: '',
+    className: 'bg-grey'
+  }
+}
 
-export const EsempioPadding: Story<EsempioInterattivoProps> = EsempioDimensioni.bind({});
-EsempioPadding.storyName = 'Esempio dimensioni';
-EsempioPadding.args = {
-  icon: 'it-check-circle',
-  padding: true,
-  color: '',
-  background: 'bg-grey'
-};
+export const EsempioColori: Story = {
+  render: ({ icon }) => {
+    return (
+      <>
+        {['primary', 'secondary', 'success', 'warning', 'danger'].map((color) => (
+          <Icon key={color} icon={icon} color={color} className='bg-grey' />
+        ))}
+        {['light', 'white'].map((color) => (
+          <Icon key={color} icon={icon} color={color} className='bg-dark' />
+        ))}
+      </>
+    );
+  },
+  args: {
+    icon: 'it-check-circle'
+  }
+}
 
-export const EsempioColori: Story<{ icon: string }> = ({ icon }) => {
-  return (
-    <>
-      {['primary', 'secondary', 'success', 'warning', 'danger'].map((color) => (
-        <Icon key={color} icon={icon} color={color} className='bg-grey' />
-      ))}
-      {['light', 'white'].map((color) => (
-        <Icon key={color} icon={icon} color={color} className='bg-dark' />
-      ))}
-    </>
-  );
-};
-EsempioColori.storyName = 'Esempio colori';
-EsempioColori.args = {
-  icon: 'it-check-circle'
-};
+export const EsempioAllineamento: Story = {
+  render: () => {
+    return (
+      <div style={{ lineHeight: '4em' }}>
+        <Icon className='bg-light align-bottom' icon='it-check-circle' />
+        <Icon className='bg-light align-middle' icon='it-check-circle' />
+        <Icon className='bg-light align-top' icon='it-check-circle' />
+      </div>
+    );
+  }
+}
 
-export const EsempioAllineamento: Story = () => {
-  return (
-    <div style={{ lineHeight: '4em' }}>
-      <Icon className='bg-light align-bottom' icon='it-check-circle' />
-      <Icon className='bg-light align-middle' icon='it-check-circle' />
-      <Icon className='bg-light align-top' icon='it-check-circle' />
-    </div>
-  );
-};
-EsempioAllineamento.storyName = 'Esempio allineamento';
+export const ListaIcone: Story = {
+  render: () => {
+    return (
+      <Row>
+        {icons.map((icon) => (
+          <Col md={{ size: 6 }} lg={{ size: 4 }} key={icon}>
+            <Icon icon={icon} />&nbsp;{icon}
+          </Col>
+        ))}
+      </Row>
+    );
+  }
+}
 
-export const ListaIcone: Story = () => {
-  return (
-    <Row>
-      {icons.map((icon) => (
-        <Col md={{ size: 6 }} lg={{ size: 4 }} key={icon}>
-          <Icon icon={icon} /> {icon}
-        </Col>
-      ))}
-    </Row>
-  );
-};
-
-ListaIcone.storyName = 'Lista icone';
-
-export const EsempioLoghiEsterni = () => {
-  return (
-    <div style={{ lineHeight: '4em' }}>
-      <Icon icon='https://upload.wikimedia.org/wikipedia/it/f/f5/Palermo-Stemma_uff.png' title='Stemma Palermo' />
-      <Icon icon='https://dati.comune.roma.it/catalog/img/Roma-Stemma.png' size='lg' title='Stemma Roma' />
-      <Icon
-        icon='https://upload.wikimedia.org/wikipedia/commons/9/93/CoA_Citt%C3%A0_di_Milano.svg'
-        size='xl'
-        title='Stemma Milano'
-      />
-    </div>
-  );
-};
+export const EsempioLoghiEsterni = {
+  render: () => {
+    return (
+      <div style={{ lineHeight: '4em' }}>
+        <Icon icon='https://upload.wikimedia.org/wikipedia/it/f/f5/Palermo-Stemma_uff.png' title='Stemma Palermo' />
+        <Icon icon='https://dati.comune.roma.it/catalog/img/Roma-Stemma.png' size='lg' title='Stemma Roma' />
+        <Icon
+          icon='https://upload.wikimedia.org/wikipedia/commons/9/93/CoA_Citt%C3%A0_di_Milano.svg'
+          size='xl'
+          title='Stemma Milano'
+        />
+      </div>
+    );
+  }
+}
