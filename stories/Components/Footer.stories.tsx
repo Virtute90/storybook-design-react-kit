a5e81ca2e7ffc4f9f400234b1c34335742e7f251
Virtute90 <matteo.ave@gmail.com>
2024-03-22 22:13:55 +0000

    Doc e Storia Footer ok

--- stories/Components/Footer.stories.tsx
+++ stories/Components/Footer.stories.tsx
@@ -1,14 +1,23 @@
 /* eslint jsx-a11y/anchor-is-valid: 0 */
+import { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
-import { Story } from '@storybook/react';
-import { Container, Row, Col, Form, Label, Input, Button, Icon, LinkList, LinkListItem } from '../../src';
+import { Button, Col, Container, Form, Icon, Input, Label, LinkList, LinkListItem, Row } from '../../src';
 
-type FooterProps = {
-  townName: string;
-  townTagLine: string;
+const meta: Meta<typeof Container> = {
+  title: "Documentazione/Componenti/Footer",
+  component: Container,
+  args: {
+    townName: 'Nome del Comune',
+    townTagLine: 'Uno dei tanti Comuni d Italia'
+  }
 };
-export const Footer: Story<FooterProps> = ({ townName, townTagLine }) => {
-  return (
+
+export default meta;
+
+type Story = StoryObj<typeof Container>;
+
+export const FooterCompleto: Story = {
+  render: ({ townName, townTagLine }) => (
     <footer className='it-footer'>
       <div className='it-footer-main'>
         <Container>
@@ -220,13 +229,135 @@ export const Footer: Story<FooterProps> = ({ townName, townTagLine }) => {
         </Container>
       </div>
     </footer>
-  );
+  )
 };
 
-export default {
-  title: 'Componenti/Footer',
-  args: {
-    townName: 'Nome del Comune',
-    townTagLine: 'Uno dei tanti Comuni d Italia'
-  }
+export const FooterCompatto: Story = {
+  render: ({ townName, townTagLine }) => (
+    <footer className='it-footer'>
+      <div className='it-footer-main'>
+        <Container>
+          <section>
+            <Row className='clearfix'>
+              <Col sm={12}>
+                <div className='it-brand-wrapper'>
+                  <a href='#' className=''>
+                    <Icon icon='it-pa' />
+                    <div className='it-brand-text'>
+                      <h2>{townName}</h2>
+                      <h3 className='d-none d-md-block'>{townTagLine}</h3>
+                    </div>
+                  </a>
+                </div>
+              </Col>
+            </Row>
+          </section>
+          <section className='py-4 border-white border-top'>
+            <Row>
+              <Col lg={4} md={4} className='pb-2'>
+                <h4>
+                  <a href='#' title='Vai alla pagina: Contatti'>
+                    Contatti
+                  </a>
+                </h4>
+                <p>
+                  <strong>Nome del Comune</strong>
+                  <br />
+                  Via Roma 0 - 00000 Lorem Ipsum Codice fiscale / P. IVA: 000000000
+                </p>
+                <LinkList className='footer-list clearfix'>
+                  {['Posta Elettronica Certificata', 'URP - Ufficio Relazioni con il Pubblico'].map((label) => (
+                    <LinkListItem key={label} href='#' title={`Vai alla pagina: ${label}`}>
+                      {label}
+                    </LinkListItem>
+                  ))}
+                </LinkList>
+              </Col>
+              <Col lg={4} md={4} className='pb-2'>
+                <h4>
+                  <a href='#' title='Vai alla pagina: Newsletter'>
+                    Newsletter
+                  </a>
+                </h4>
+                <Form action='#' className='form-newsletter' method='post'>
+                  <Label
+                    className='text-white fw-semibold active'
+                    htmlFor='input-newsletter'
+                    style={{ transition: 'none 0s ease 0s' }}
+                  >
+                    Iscriviti per riceverla
+                  </Label>
+                  <Input id='input-newsletter' name='input-newsletter' placeholder='mail@example.com' type='email' />
+                  <Button color='primary' className='btn-icon' type='submit'>
+                    <Icon icon='it-mail' color='white' />
+                    <span>Iscriviti</span>
+                  </Button>
+                </Form>
+              </Col>
+              <Col lg={4} md={4} className='pb-2'>
+                <h4>
+                  <a href='#' title='Vai alla pagina: Seguici su'>
+                    Seguici su
+                  </a>
+                </h4>
+                <ul className='list-inline text-start social'>
+                  <li className='list-inline-item'>
+                    <a className='p-2 text-white' href='#' target='_blank'>
+                      <Icon icon='it-designers-italia' color='white' size='sm' className='align-top' />
+                      <span className='visually-hidden'>Designers Italia</span>
+                    </a>
+                  </li>
+                  <li className='list-inline-item'>
+                    <a className='p-2 text-white' href='#' target='_blank'>
+                      <Icon icon='it-twitter' color='white' size='sm' className='align-top' />
+                      <span className='visually-hidden'>Twitter</span>
+                    </a>
+                  </li>
+                  <li className='list-inline-item'>
+                    <a className='p-2 text-white' href='#' target='_blank'>
+                      <Icon icon='it-medium' color='white' size='sm' className='align-top' />
+                      <span className='visually-hidden'>Medium</span>
+                    </a>
+                  </li>
+                  <li className='list-inline-item'>
+                    <a className='p-2 text-white' href='#' target='_blank'>
+                      <Icon icon='it-behance' color='white' size='sm' className='align-top' />
+                      <span className='visually-hidden'>Behance</span>
+                    </a>
+                  </li>
+                </ul>
+              </Col>
+            </Row>
+          </section>
+        </Container>
+      </div>
+      <div className='it-footer-small-prints clearfix'>
+        <Container>
+          <h3 className='visually-hidden'>Sezione Link Utili</h3>
+          <ul className='it-footer-small-prints-list list-inline mb-0 d-flex flex-column flex-md-row'>
+            <li className='list-inline-item'>
+              <a href='#' title='Note Legali'>
+                Media policy
+              </a>
+            </li>
+            <li className='list-inline-item'>
+              <a href='#' title='Note Legali'>
+                Note legali
+              </a>
+            </li>
+            <li className='list-inline-item'>
+              <a href='#' title='Privacy-Cookies'>
+                Privacy policy
+              </a>
+            </li>
+            <li className='list-inline-item'>
+              <a href='#' title='Mappa del sito'>
+                Mappa del sito
+              </a>
+            </li>
+          </ul>
+        </Container>
+      </div>
+    </footer>
+  )
 };
