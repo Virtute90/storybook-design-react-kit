0d45b5139b3b2dd6ebae225e4fcd77cbc0a0ffa1
Virtute90 <matteo.ave@gmail.com>
2024-03-23 14:18:02 +0000

    Doc e storie Dropdown ok

--- stories/Components/Dropdown/Dropdown.stories.tsx
+++ stories/Components/Dropdown/Dropdown.stories.tsx
@@ -1,88 +1,99 @@
+import { Meta, StoryObj } from "@storybook/react";
 import React from "react";
+import { ButtonGroup, Dropdown, DropdownMenu, DropdownToggle, LinkList, LinkListItem } from "../../../src";
 
-import { Dropdown, DropdownMenu, DropdownToggle, LinkList, LinkListItem, ButtonGroup } from "../../../src";
-
-export const _VarianteBottoni = () => {
-    return (
-        <ButtonGroup>
-            <Dropdown>
-                <DropdownToggle color="primary" caret>
-                    Apri dropdown
-                </DropdownToggle>
-                <DropdownMenu>
-                    <LinkList>
-                        <LinkListItem inDropdown>Azione 1</LinkListItem>
-                        <LinkListItem inDropdown>Azione 2</LinkListItem>
-                        <LinkListItem inDropdown>Azione 3</LinkListItem>
-                    </LinkList>
-                </DropdownMenu>
-            </Dropdown>
-            <Dropdown>
-                <DropdownToggle color="secondary" caret>
-                    Apri dropdown
-                </DropdownToggle>
-                <DropdownMenu>
-                    <LinkList>
-                        <LinkListItem inDropdown>Azione 1</LinkListItem>
-                        <LinkListItem inDropdown>Azione 2</LinkListItem>
-                        <LinkListItem inDropdown>Azione 3</LinkListItem>
-                    </LinkList>
-                </DropdownMenu>
-            </Dropdown>
-            <Dropdown>
-                <DropdownToggle color="danger" caret>
-                    Apri dropdown
-                </DropdownToggle>
-                <DropdownMenu>
-                    <LinkList>
-                        <LinkListItem>Azione 1</LinkListItem>
-                        <LinkListItem>Azione 2</LinkListItem>
-                        <LinkListItem>Azione 3</LinkListItem>
-                    </LinkList>
-                </DropdownMenu>
-            </Dropdown>
-        </ButtonGroup>
-    );
+const meta: Meta<typeof Dropdown> = {
+    title: "Documentazione/Componenti/Dropdown",
+    component: Dropdown,
 };
 
-export default {
-    title: "Componenti/Dropdown",
+export default meta;
+
+type Story = StoryObj<typeof Dropdown>;
+
+export const _VarianteBottoni: Story = {
+    render: () => {
+        return (
+            <ButtonGroup className="d-flex justify-content-around">
+                <Dropdown>
+                    <DropdownToggle color="primary" caret>
+                        Apri dropdown
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <LinkList>
+                            <LinkListItem inDropdown>Azione 1</LinkListItem>
+                            <LinkListItem inDropdown>Azione 2</LinkListItem>
+                            <LinkListItem inDropdown>Azione 3</LinkListItem>
+                        </LinkList>
+                    </DropdownMenu>
+                </Dropdown>
+                <Dropdown>
+                    <DropdownToggle color="secondary" caret>
+                        Apri dropdown
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <LinkList>
+                            <LinkListItem inDropdown>Azione 1</LinkListItem>
+                            <LinkListItem inDropdown>Azione 2</LinkListItem>
+                            <LinkListItem inDropdown>Azione 3</LinkListItem>
+                        </LinkList>
+                    </DropdownMenu>
+                </Dropdown>
+                <Dropdown>
+                    <DropdownToggle color="danger" caret>
+                        Apri dropdown
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <LinkList>
+                            <LinkListItem>Azione 1</LinkListItem>
+                            <LinkListItem>Azione 2</LinkListItem>
+                            <LinkListItem>Azione 3</LinkListItem>
+                        </LinkList>
+                    </DropdownMenu>
+                </Dropdown>
+            </ButtonGroup>
+        );
+    }
 };
 
-export const _Esempi = () => {
-    return (
-        <section>
-            <Dropdown className="me-3">
-                <DropdownToggle color="primary" caret>
-                    Apri dropdown
-                </DropdownToggle>
-                <DropdownMenu>
-                    <LinkList>
-                        <LinkListItem inDropdown>Azione 1</LinkListItem>
-                        <LinkListItem inDropdown>Azione 2</LinkListItem>
-                        <LinkListItem inDropdown>Azione 3</LinkListItem>
-                    </LinkList>
-                </DropdownMenu>
-            </Dropdown>
-        </section>
-    );
+export const _Esempi: Story = {
+    render: () => {
+        return (
+            <section>
+                <Dropdown className="me-3">
+                    <DropdownToggle color="primary" caret>
+                        Apri dropdown
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <LinkList>
+                            <LinkListItem inDropdown>Azione 1</LinkListItem>
+                            <LinkListItem inDropdown>Azione 2</LinkListItem>
+                            <LinkListItem inDropdown>Azione 3</LinkListItem>
+                        </LinkList>
+                    </DropdownMenu>
+                </Dropdown>
+            </section>
+        );
+    }
 };
 
-export const _Link = () => {
-    return (
-        <section>
-            <Dropdown className="me-3">
-                <DropdownToggle color="primary" tag="a" caret>
-                    Apri dropdown
-                </DropdownToggle>
-                <DropdownMenu>
-                    <LinkList>
-                        <LinkListItem inDropdown>Azione 1</LinkListItem>
-                        <LinkListItem inDropdown>Azione 2</LinkListItem>
-                        <LinkListItem inDropdown>Azione 3</LinkListItem>
-                    </LinkList>
-                </DropdownMenu>
-            </Dropdown>
-        </section>
-    );
+export const _Link: Story = {
+    render: () => {
+        return (
+            <section>
+                <Dropdown className="me-3">
+                    <DropdownToggle color="primary" tag="a" caret>
+                        Apri dropdown
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <LinkList>
+                            <LinkListItem inDropdown>Azione 1</LinkListItem>
+                            <LinkListItem inDropdown>Azione 2</LinkListItem>
+                            <LinkListItem inDropdown>Azione 3</LinkListItem>
+                        </LinkList>
+                    </DropdownMenu>
+                </Dropdown>
+            </section>
+        );
+    }
 };
--- stories/Components/Dropdown/DropdownMenu.stories.tsx
+++ stories/Components/Dropdown/DropdownMenu.stories.tsx
@@ -1,201 +1,206 @@
+import { Meta, StoryObj } from "@storybook/react";
 import React from "react";
-
 import { DropdownMenu, Icon, LinkList, LinkListItem } from "../../../src";
 
-export const _MenuVociAttive = () => (
-    <div className="docs-show-dropdown-open">
-        <DropdownMenu>
-            <LinkList>
-                <LinkListItem active inDropdown>
-                    <span>
-                        Azione 1<span className="visually-hidden"> attivo</span>
-                    </span>
-                </LinkListItem>
-                <LinkListItem inDropdown>
-                    <span>Azione 2</span>
-                </LinkListItem>
-                <LinkListItem inDropdown>
-                    <span>Azione 3</span>
-                </LinkListItem>
-            </LinkList>
-        </DropdownMenu>
-    </div>
-);
-
-export const _MenuVociDisabilitate = () => (
-    <div className="docs-show-dropdown-open">
-        <DropdownMenu>
-            <LinkList>
-                <LinkListItem inDropdown>
-                    <span>Azione 1</span>
-                </LinkListItem>
-                <LinkListItem disabled aria-disabled="true" inDropdown>
-                    <span>Azione 2</span>
-                </LinkListItem>
-                <LinkListItem inDropdown>
-                    <span>Azione 3</span>
-                </LinkListItem>
-            </LinkList>
-        </DropdownMenu>
-    </div>
-);
-
-export const MenuHeadersESeparatori = () => (
-    <div className="docs-show-dropdown-open">
-        <DropdownMenu>
-            <LinkList>
-                <LinkListItem header inDropdown>
-                    Header
-                </LinkListItem>
-                <LinkListItem>
-                    <span>Azione 1</span>
-                </LinkListItem>
-                <LinkListItem inDropdown>
-                    <span>Azione 2</span>
-                </LinkListItem>
-                <LinkListItem inDropdown>
-                    <span>Azione 3</span>
-                </LinkListItem>
-                <LinkListItem divider />
-                <LinkListItem inDropdown>
-                    <span>Azione 4</span>
-                </LinkListItem>
-            </LinkList>
-        </DropdownMenu>
-    </div>
-);
-
-export const MenuConVociLarge = () => (
-    <div className="docs-show-dropdown-open">
-        <DropdownMenu>
-            <LinkList>
-                <LinkListItem large inDropdown>
-                    <span>Azione 1</span>
-                </LinkListItem>
-                <LinkListItem large inDropdown>
-                    <span>Azione 2</span>
-                </LinkListItem>
-                <LinkListItem large inDropdown>
-                    <span>Azione 3</span>
-                </LinkListItem>
-            </LinkList>
-        </DropdownMenu>
-    </div>
-);
-
-export const MenuFullWidth = () => (
-    <div className="docs-show-dropdown-open">
-        <DropdownMenu className="full-width">
-            <LinkList>
-                <LinkListItem large inDropdown>
-                    <span>Azione 1</span>
-                </LinkListItem>
-                <LinkListItem large inDropdown>
-                    <span>Azione 2</span>
-                </LinkListItem>
-                <LinkListItem large inDropdown>
-                    <span>Azione 3</span>
-                </LinkListItem>
-                <LinkListItem large inDropdown>
-                    <span>Azione 4</span>
-                </LinkListItem>
-                <LinkListItem large inDropdown>
-                    <span>Azione 5</span>
-                </LinkListItem>
-            </LinkList>
-        </DropdownMenu>
-    </div>
-);
-
-export const MenuIconaADestra = () => (
-    <div className="docs-show-dropdown-open">
-        <DropdownMenu>
-            <LinkList>
-                <LinkListItem className="right-icon" inDropdown>
-                    <span>Azione 1</span>
-                    <Icon className="right" color="primary" icon="it-info-circle" aria-hidden size="sm" />
-                </LinkListItem>
-                <LinkListItem className="right-icon" inDropdown>
-                    <span>Azione 2</span>
-                    <Icon className="right" color="primary" icon="it-info-circle" aria-hidden size="sm" />
-                </LinkListItem>
-                <LinkListItem className="right-icon" inDropdown>
-                    <span>Azione 3</span>
-                    <Icon className="right" color="primary" icon="it-info-circle" aria-hidden size="sm" />
-                </LinkListItem>
-            </LinkList>
-        </DropdownMenu>
-    </div>
-);
-
-export const MenuIconaASinistra = () => (
-    <div className="docs-show-dropdown-open">
-        <DropdownMenu>
-            <LinkList>
-                <LinkListItem className="left-icon" inDropdown>
-                    <Icon className="left" color="primary" icon="it-info-circle" aria-hidden size="sm" />
-                    <span>Azione 1</span>
-                </LinkListItem>
-                <LinkListItem className="left-icon" inDropdown>
-                    <Icon className="left" color="primary" icon="it-info-circle" aria-hidden size="sm" />
-                    <span>Azione 2</span>
-                </LinkListItem>
-                <LinkListItem className="left-icon" inDropdown>
-                    <Icon className="left" color="primary" icon="it-info-circle" aria-hidden size="sm" />
-                    <span>Azione 3</span>
-                </LinkListItem>
-            </LinkList>
-        </DropdownMenu>
-    </div>
-);
-
-export const _MenuDark = () => (
-    <div className="docs-show-dropdown-open">
-        <DropdownMenu className="dark">
-            <LinkList>
-                <LinkListItem header>Header</LinkListItem>
-                <LinkListItem active className="right-icon" inDropdown>
-                    <span>Azione 1 (attivo)</span>
-                    <Icon className="right" color="light" icon="it-info-circle" aria-hidden size="sm" />
-                </LinkListItem>
-                <LinkListItem className="right-icon" inDropdown>
-                    <span>Azione 2</span>
-                    <Icon className="right" color="light" icon="it-info-circle" aria-hidden size="sm" />
-                </LinkListItem>
-                <LinkListItem className="right-icon" inDropdown>
-                    <span>Azione 3</span>
-                    <Icon className="right" color="light" icon="it-info-circle" aria-hidden size="sm" />
-                </LinkListItem>
-                <LinkListItem divider />
-                <LinkListItem className="right-icon" inDropdown>
-                    <span>Azione 4</span>
-                    <Icon className="right" color="light" icon="it-info-circle" aria-hidden size="sm" />
-                </LinkListItem>
-                <LinkListItem disabled className="right-icon" inDropdown>
-                    <span>Azione 5 (disabilitato)</span>
-                    <Icon className="right" color="light" icon="it-info-circle" aria-hidden size="sm" />
-                </LinkListItem>
-            </LinkList>
-        </DropdownMenu>
-    </div>
-);
-
-export default {
-    title: "Componenti/Dropdown/Dropdown menu",
+const meta: Meta<typeof DropdownMenu> = {
+    title: "Documentazione/Componenti/Dropdown",
+    component: DropdownMenu,
 };
 
-_MenuVociAttive.storyName = "Menù voci attive";
+export default meta;
 
-_MenuVociDisabilitate.storyName = "Menù voci disabilitate";
+type Story = StoryObj<typeof DropdownMenu>;
 
-MenuHeadersESeparatori.storyName = "Menù headers e separatori";
+export const _MenuVociAttive: Story = {
+    render: () => (
+        <div className="docs-show-dropdown-open">
+            <DropdownMenu>
+                <LinkList>
+                    <LinkListItem active inDropdown>
+                        <span>
+                            Azione 1<span className="visually-hidden"> attivo</span>
+                        </span>
+                    </LinkListItem>
+                    <LinkListItem inDropdown>
+                        <span>Azione 2</span>
+                    </LinkListItem>
+                    <LinkListItem inDropdown>
+                        <span>Azione 3</span>
+                    </LinkListItem>
+                </LinkList>
+            </DropdownMenu>
+        </div>
+    )
+};
 
-MenuConVociLarge.storyName = "Menu con voci large";
+export const _MenuVociDisabilitate: Story = {
+    render: () => (
+        <div className="docs-show-dropdown-open">
+            <DropdownMenu>
+                <LinkList>
+                    <LinkListItem inDropdown>
+                        <span>Azione 1</span>
+                    </LinkListItem>
+                    <LinkListItem disabled aria-disabled="true" inDropdown>
+                        <span>Azione 2</span>
+                    </LinkListItem>
+                    <LinkListItem inDropdown>
+                        <span>Azione 3</span>
+                    </LinkListItem>
+                </LinkList>
+            </DropdownMenu>
+        </div>
+    )
+};
 
-MenuFullWidth.storyName = "Menù full width";
+export const MenuHeadersESeparatori: Story = {
+    render: () => (
+        <div className="docs-show-dropdown-open">
+            <DropdownMenu>
+                <LinkList>
+                    <LinkListItem header inDropdown>
+                        Header
+                    </LinkListItem>
+                    <LinkListItem>
+                        <span>Azione 1</span>
+                    </LinkListItem>
+                    <LinkListItem inDropdown>
+                        <span>Azione 2</span>
+                    </LinkListItem>
+                    <LinkListItem inDropdown>
+                        <span>Azione 3</span>
+                    </LinkListItem>
+                    <LinkListItem divider />
+                    <LinkListItem inDropdown>
+                        <span>Azione 4</span>
+                    </LinkListItem>
+                </LinkList>
+            </DropdownMenu>
+        </div>
+    )
+};
 
-MenuIconaADestra.storyName = "Menù icona a destra";
+export const MenuConVociLarge: Story = {
+    render: () => (
+        <div className="docs-show-dropdown-open">
+            <DropdownMenu>
+                <LinkList>
+                    <LinkListItem large inDropdown>
+                        <span>Azione 1</span>
+                    </LinkListItem>
+                    <LinkListItem large inDropdown>
+                        <span>Azione 2</span>
+                    </LinkListItem>
+                    <LinkListItem large inDropdown>
+                        <span>Azione 3</span>
+                    </LinkListItem>
+                </LinkList>
+            </DropdownMenu>
+        </div>
+    )
+};
 
-MenuIconaASinistra.storyName = "Menù icona a sinistra";
+export const MenuFullWidth: Story = {
+    render: () => (
+        <div className="docs-show-dropdown-open">
+            <DropdownMenu className="full-width">
+                <LinkList>
+                    <LinkListItem large inDropdown>
+                        <span>Azione 1</span>
+                    </LinkListItem>
+                    <LinkListItem large inDropdown>
+                        <span>Azione 2</span>
+                    </LinkListItem>
+                    <LinkListItem large inDropdown>
+                        <span>Azione 3</span>
+                    </LinkListItem>
+                    <LinkListItem large inDropdown>
+                        <span>Azione 4</span>
+                    </LinkListItem>
+                    <LinkListItem large inDropdown>
+                        <span>Azione 5</span>
+                    </LinkListItem>
+                </LinkList>
+            </DropdownMenu>
+        </div>
+    )
+};
 
-_MenuDark.storyName = "Menù dark";
+export const MenuIconaADestra: Story = {
+    render: () => (
+        <div className="docs-show-dropdown-open">
+            <DropdownMenu>
+                <LinkList>
+                    <LinkListItem className="right-icon" inDropdown>
+                        <span>Azione 1</span>
+                        <Icon className="right" color="primary" icon="it-info-circle" aria-hidden size="sm" />
+                    </LinkListItem>
+                    <LinkListItem className="right-icon" inDropdown>
+                        <span>Azione 2</span>
+                        <Icon className="right" color="primary" icon="it-info-circle" aria-hidden size="sm" />
+                    </LinkListItem>
+                    <LinkListItem className="right-icon" inDropdown>
+                        <span>Azione 3</span>
+                        <Icon className="right" color="primary" icon="it-info-circle" aria-hidden size="sm" />
+                    </LinkListItem>
+                </LinkList>
+            </DropdownMenu>
+        </div>
+    )
+};
+
+export const MenuIconaASinistra: Story = {
+    render: () => (
+        <div className="docs-show-dropdown-open">
+            <DropdownMenu>
+                <LinkList>
+                    <LinkListItem className="left-icon" inDropdown>
+                        <Icon className="left" color="primary" icon="it-info-circle" aria-hidden size="sm" />
+                        <span>Azione 1</span>
+                    </LinkListItem>
+                    <LinkListItem className="left-icon" inDropdown>
+                        <Icon className="left" color="primary" icon="it-info-circle" aria-hidden size="sm" />
+                        <span>Azione 2</span>
+                    </LinkListItem>
+                    <LinkListItem className="left-icon" inDropdown>
+                        <Icon className="left" color="primary" icon="it-info-circle" aria-hidden size="sm" />
+                        <span>Azione 3</span>
+                    </LinkListItem>
+                </LinkList>
+            </DropdownMenu>
+        </div>
+    )
+};
+
+export const _MenuDark: Story = {
+    render: () => (
+        <div className="docs-show-dropdown-open">
+            <DropdownMenu className="dark">
+                <LinkList>
+                    <LinkListItem header>Header</LinkListItem>
+                    <LinkListItem active className="right-icon" inDropdown>
+                        <span>Azione 1 (attivo)</span>
+                        <Icon className="right" color="light" icon="it-info-circle" aria-hidden size="sm" />
+                    </LinkListItem>
+                    <LinkListItem className="right-icon" inDropdown>
+                        <span>Azione 2</span>
+                        <Icon className="right" color="light" icon="it-info-circle" aria-hidden size="sm" />
+                    </LinkListItem>
+                    <LinkListItem className="right-icon" inDropdown>
+                        <span>Azione 3</span>
+                        <Icon className="right" color="light" icon="it-info-circle" aria-hidden size="sm" />
+                    </LinkListItem>
+                    <LinkListItem divider />
+                    <LinkListItem className="right-icon" inDropdown>
+                        <span>Azione 4</span>
+                        <Icon className="right" color="light" icon="it-info-circle" aria-hidden size="sm" />
+                    </LinkListItem>
+                    <LinkListItem disabled className="right-icon" inDropdown>
+                        <span>Azione 5 (disabilitato)</span>
+                        <Icon className="right" color="light" icon="it-info-circle" aria-hidden size="sm" />
+                    </LinkListItem>
+                </LinkList>
+            </DropdownMenu>
+        </div>
+    )
+};
