9505025bdfbf84d778b1b22ae4a00d9bd60c00b9
Virtute90 <matteo.ave@gmail.com>
2024-03-25 22:19:52 +0000

    Formattatato documento

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,23 @@
-import { StorybookConfig } from "@storybook/react-vite";
+import { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-    stories: ["../stories/**/*.@(mdx|stories.@(ts|tsx|js|jsx))", "../stories/**/*.mdx"],
-    addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y"],
-    framework: {
-        name: "@storybook/react-vite",
-        options: {},
-    },
-    core: {},
-    staticDirs: ["../static", { from: "../assets", to: "/" }],
-    docs: {
-        autodocs: false,
-        defaultName: "Documentazione",
-        docsMode: true,
-    },
+  stories: ['../stories/**/*.@(mdx|stories.@(ts|tsx|js|jsx))', '../stories/**/*.mdx'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y'
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {}
+  },
+  core: {},
+  staticDirs: ['../static', { from: '../assets', to: '/' }],
+  docs: {
+    autodocs: false,
+    defaultName: 'Documentazione',
+    docsMode: true
+  }
 };
 export default config;
