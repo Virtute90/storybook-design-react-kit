cb6d9f4267ae2c76f86d7e7b802ca00ce179b4df
Virtute90 <matteo.ave@gmail.com>
2024-03-25 21:59:40 +0000

    Doc e storia popover ok

--- stories/Components/Popover.stories.tsx
+++ stories/Components/Popover.stories.tsx
@@ -1,259 +1,266 @@
-/* eslint jsx-a11y/anchor-is-valid: 0 */
-import React, { useState, useRef } from 'react';
-import { Meta, Story } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
+import React, { useRef, useState } from 'react';
+import { Button, Icon, Popover, PopoverBody, PopoverHeader } from '../../src';
 
-import { Popover, PopoverHeader, PopoverBody, Button, Icon } from '../../src';
-
-const meta: Meta = {
-  title: 'Componenti/Popover',
-  component: Popover
+const meta: Meta<typeof Popover> = {
+  title: "Documentazione/Componenti/Popover",
+  component: Popover,
+  parameters: {
+    docs: {
+      canvas: { sourceState: "none" },
+    },
+  },
 };
+
 export default meta;
 
-const PopoverExample = () => {
-  const [popoverOpen, setPopoverOpen] = useState(false);
-  const targetRef = useRef(null);
+type Story = StoryObj<typeof Popover>;
 
-  const togglePopover = () => {
-    setPopoverOpen(!popoverOpen);
-  };
+export const Example: Story = {
+  render: () => {
+    const [popoverOpen, setPopoverOpen] = useState(false);
+    const targetRef = useRef(null);
 
-  return (
-    <div>
-      <Button color='danger' size='lg' innerRef={targetRef} onClick={togglePopover}>
-        Clicca per attivare/disattivare il popover
-      </Button>
+    const togglePopover = () => {
+      setPopoverOpen(!popoverOpen);
+    };
 
-      <Popover placement='right' target={targetRef} isOpen={popoverOpen} toggle={togglePopover}>
-        <PopoverHeader>Titolo del popover</PopoverHeader>
-        <PopoverBody>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel finibus augue.</PopoverBody>
-      </Popover>
-    </div>
-  );
-};
-
-export const _Esempi: Story = PopoverExample.bind({});
-
-const ElementiDisabilitatiComponent = () => {
-  const [popoverOpen, setPopoverOpen] = useState(false);
-  const targetRef = useRef(null);
-
-  return (
-    <>
-      <span ref={targetRef}>
-        <Button color='primary' disabled style={{ pointerEvents: 'none' }}>
-          Popover disabilitato
+    return (
+      <div>
+        <Button color='danger' size='lg' innerRef={targetRef} onClick={togglePopover}>
+          Clicca per attivare/disattivare il popover
         </Button>
-      </span>
-      <Popover
-        placement='right'
-        target={targetRef}
-        trigger='hover'
-        toggle={() => setPopoverOpen(!popoverOpen)}
-        isOpen={popoverOpen}
-      >
-        <PopoverHeader>Titolo del popover</PopoverHeader>
-        <PopoverBody>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel finibus augue.</PopoverBody>
-      </Popover>
-    </>
-  );
-};
-export const _ElementiDisabilitati: Story = ElementiDisabilitatiComponent.bind({});
-_ElementiDisabilitati.storyName = 'Elementi disabilitati';
 
-const PopoverPositionExample = () => {
-  const [popoverOpen1, setPopoverOpen1] = useState(false);
-  const [popoverOpen2, setPopoverOpen2] = useState(false);
-  const [popoverOpen3, setPopoverOpen3] = useState(false);
-  const [popoverOpen4, setPopoverOpen4] = useState(false);
-
-  const targetRef1 = useRef(null);
-  const targetRef2 = useRef(null);
-  const targetRef3 = useRef(null);
-  const targetRef4 = useRef(null);
-
-  return (
-    <div className='d-flex flex-column'>
-      <div className='mx-auto align-self-center p-2'>
-        <span>
-          <Button
-            className='me-1'
-            color='secondary'
-            innerRef={targetRef1}
-            onClick={() => {
-              setPopoverOpen1(!popoverOpen1);
-            }}
-          >
-            Popover {'a sinistra'}
-          </Button>
-          <Popover
-            placement={'left'}
-            isOpen={popoverOpen1}
-            target={targetRef1}
-            toggle={() => {
-              setPopoverOpen1(!popoverOpen1);
-            }}
-          >
-            <PopoverBody>Vivamus sagittis lacus vel augue laoreet rutrum faucibus</PopoverBody>
-          </Popover>
-        </span>
+        <Popover placement='right' target={targetRef} isOpen={popoverOpen} toggle={togglePopover}>
+          <PopoverHeader>Titolo del popover</PopoverHeader>
+          <PopoverBody>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel finibus augue.</PopoverBody>
+        </Popover>
       </div>
-      <div className='mx-auto align-self-center p-2'>
-        <span>
-          <Button
-            className='me-1'
-            color='secondary'
-            innerRef={targetRef2}
-            onClick={() => {
-              setPopoverOpen2(!popoverOpen2);
-            }}
-          >
-            Popover {'a destra'}
+    );
+  }
+};
+
+export const ElementiDisabilitati: Story = {
+  render: () => {
+    const [popoverOpen, setPopoverOpen] = useState(false);
+    const targetRef = useRef(null);
+
+    return (
+      <>
+        <span ref={targetRef}>
+          <Button color='primary' disabled style={{ pointerEvents: 'none' }}>
+            Popover disabilitato
           </Button>
-          <Popover
-            placement={'right'}
-            isOpen={popoverOpen2}
-            target={targetRef2}
-            toggle={() => {
-              setPopoverOpen2(!popoverOpen2);
-            }}
-          >
-            <PopoverBody>Vivamus sagittis lacus vel augue laoreet rutrum faucibus</PopoverBody>
-          </Popover>
         </span>
+        <Popover
+          placement='right'
+          target={targetRef}
+          trigger='hover'
+          toggle={() => setPopoverOpen(!popoverOpen)}
+          isOpen={popoverOpen}
+        >
+          <PopoverHeader>Titolo del popover</PopoverHeader>
+          <PopoverBody>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel finibus augue.</PopoverBody>
+        </Popover>
+      </>
+    );
+  }
+};
+
+export const Direzioni: Story = {
+  render: () => {
+    const [popoverOpenSx, setPopoverOpenSx] = useState(false);
+    const [popoverOpenDx, setPopoverOpenDx] = useState(false);
+    const [popoverOpenAlto, setPopoverOpenAlto] = useState(false);
+    const [popoverOpenBasso, setPopoverOpenBasso] = useState(false);
+
+    const targetRefSx = useRef(null);
+    const targetRefDx = useRef(null);
+    const targetRefAlto = useRef(null);
+    const targetRefBasso = useRef(null);
+
+    return (
+      <div className='d-flex flex-column'>
+        <div className='mx-auto align-self-center p-2'>
+          <span>
+            <Button
+              className='me-1'
+              color='secondary'
+              innerRef={targetRefSx}
+              onClick={() => {
+                setPopoverOpenSx(!popoverOpenSx);
+              }}
+            >
+              Popover {'a sinistra'}
+            </Button>
+            <Popover
+              placement={'left'}
+              isOpen={popoverOpenSx}
+              target={targetRefSx}
+              toggle={() => {
+                setPopoverOpenSx(!popoverOpenSx);
+              }}
+            >
+              <PopoverBody>Vivamus sagittis lacus vel augue laoreet rutrum faucibus</PopoverBody>
+            </Popover>
+          </span>
+        </div>
+        <div className='mx-auto align-self-center p-2'>
+          <span>
+            <Button
+              className='me-1'
+              color='secondary'
+              innerRef={targetRefDx}
+              onClick={() => {
+                setPopoverOpenDx(!popoverOpenDx);
+              }}
+            >
+              Popover {'a destra'}
+            </Button>
+            <Popover
+              placement={'right'}
+              isOpen={popoverOpenDx}
+              target={targetRefDx}
+              toggle={() => {
+                setPopoverOpenDx(!popoverOpenDx);
+              }}
+            >
+              <PopoverBody>Vivamus sagittis lacus vel augue laoreet rutrum faucibus</PopoverBody>
+            </Popover>
+          </span>
+        </div>
+        <div className='mx-auto align-self-center p-2'>
+          <span>
+            <Button
+              className='me-1'
+              color='secondary'
+              innerRef={targetRefAlto}
+              onClick={() => {
+                setPopoverOpenAlto(!popoverOpenAlto);
+              }}
+            >
+              Popover {'in alto'}
+            </Button>
+            <Popover
+              placement={'top'}
+              isOpen={popoverOpenAlto}
+              target={targetRefAlto}
+              toggle={() => {
+                setPopoverOpenAlto(!popoverOpenAlto);
+              }}
+            >
+              <PopoverBody>Vivamus sagittis lacus vel augue laoreet rutrum faucibus</PopoverBody>
+            </Popover>
+          </span>
+        </div>
+        <div className='mx-auto align-self-center p-2'>
+          <span>
+            <Button
+              className='me-1'
+              color='secondary'
+              innerRef={targetRefBasso}
+              onClick={() => {
+                setPopoverOpenBasso(!popoverOpenBasso);
+              }}
+            >
+              Popover {'in basso'}
+            </Button>
+            <Popover
+              placement={'bottom'}
+              isOpen={popoverOpenBasso}
+              target={targetRefBasso}
+              toggle={() => {
+                setPopoverOpenBasso(!popoverOpenBasso);
+              }}
+            >
+              <PopoverBody>Vivamus sagittis lacus vel augue laoreet rutrum faucibus</PopoverBody>
+            </Popover>
+          </span>
+        </div>
       </div>
-      <div className='mx-auto align-self-center p-2'>
-        <span>
-          <Button
-            className='me-1'
-            color='secondary'
-            innerRef={targetRef3}
-            onClick={() => {
-              setPopoverOpen3(!popoverOpen3);
-            }}
-          >
-            Popover {'in alto'}
-          </Button>
-          <Popover
-            placement={'top'}
-            isOpen={popoverOpen3}
-            target={targetRef3}
-            toggle={() => {
-              setPopoverOpen3(!popoverOpen3);
-            }}
-          >
-            <PopoverBody>Vivamus sagittis lacus vel augue laoreet rutrum faucibus</PopoverBody>
-          </Popover>
-        </span>
+    );
+  }
+};
+
+export const TitoloIconaLink: Story = {
+  render: () => {
+    const [popoverOpen, setPopoverOpen] = useState(false);
+    const targetRef = useRef(null);
+
+    const togglePopover = () => {
+      setPopoverOpen(!popoverOpen);
+    };
+    return (
+      <div>
+        <Button color='secondary' innerRef={targetRef} onClick={togglePopover}>
+          Popover con icona e link
+        </Button>
+
+        <Popover placement='right' target={targetRef} isOpen={popoverOpen} toggle={togglePopover}>
+          <PopoverHeader>
+            <Icon icon='it-help-circle' aria-hidden />
+            Titolo con icona
+          </PopoverHeader>
+          <PopoverBody>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel finibus augue.
+            <a href='#' className='popover-inner-link'>
+              More info
+              <Icon icon='it-arrow-right' aria-hidden />
+            </a>
+          </PopoverBody>
+        </Popover>
       </div>
-      <div className='mx-auto align-self-center p-2'>
-        <span>
-          <Button
-            className='me-1'
-            color='secondary'
-            innerRef={targetRef4}
-            onClick={() => {
-              setPopoverOpen4(!popoverOpen4);
-            }}
-          >
-            Popover {'in basso'}
-          </Button>
-          <Popover
-            placement={'bottom'}
-            isOpen={popoverOpen4}
-            target={targetRef4}
-            toggle={() => {
-              setPopoverOpen4(!popoverOpen4);
-            }}
-          >
-            <PopoverBody>Vivamus sagittis lacus vel augue laoreet rutrum faucibus</PopoverBody>
-          </Popover>
-        </span>
+    );
+  }
+};
+
+export const ModalitàHover: Story = {
+  render: () => {
+    const [popoverOpen, setPopoverOpen] = useState(false);
+    const targetRef = useRef(null);
+
+    const togglePopover = () => {
+      setPopoverOpen(!popoverOpen);
+    };
+    return (
+      <div>
+        <Button color='secondary' innerRef={targetRef}>
+          Apertura in Hover
+        </Button>
+
+        <Popover placement='right' trigger='hover' target={targetRef} isOpen={popoverOpen} toggle={togglePopover}>
+          <PopoverHeader>Popover in Hover</PopoverHeader>
+          <PopoverBody>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel finibus augue.</PopoverBody>
+        </Popover>
       </div>
-    </div>
-  );
+    );
+  }
 };
-export const _LeQuattroDirezioni: Story = PopoverPositionExample.bind({});
-_LeQuattroDirezioni.storyName = 'Le quattro direzioni';
 
-const PopoverIconLink = () => {
-  const [popoverOpen, setPopoverOpen] = useState(false);
-  const targetRef = useRef(null);
+export const ClickSuccessivo: Story = {
+  render: () => {
+    const [popoverOpen, setPopoverOpen] = useState(false);
+    const targetRef = useRef(null);
 
-  const togglePopover = () => {
-    setPopoverOpen(!popoverOpen);
-  };
-  return (
-    <div>
-      <Button color='secondary' innerRef={targetRef} onClick={togglePopover}>
-        Popover con icona e link
-      </Button>
+    const togglePopover = () => {
+      setPopoverOpen(!popoverOpen);
+    };
+    return (
+      <div>
+        <Button tabIndex={0} color='danger' innerRef={targetRef}>
+          Popover richiudibile
+        </Button>
 
-      <Popover placement='right' target={targetRef} isOpen={popoverOpen} toggle={togglePopover}>
-        <PopoverHeader>
-          <Icon icon='it-help-circle' aria-hidden />
-          Titolo con icona
-        </PopoverHeader>
-        <PopoverBody>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel finibus augue.
-          <a href='#' className='popover-inner-link'>
-            More info
-            <Icon icon='it-arrow-right' aria-hidden />
-          </a>
-        </PopoverBody>
-      </Popover>
-    </div>
-  );
+        <Popover placement='right' trigger='focus' target={targetRef} isOpen={popoverOpen} toggle={togglePopover}>
+          <PopoverHeader>Dismissible popover</PopoverHeader>
+          <PopoverBody>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel finibus augue.</PopoverBody>
+        </Popover>
+      </div>
+    );
+  }
 };
-export const _TitoloConIconaELink: Story = PopoverIconLink.bind({});
-_TitoloConIconaELink.storyName = 'Titolo con icona e link';
 
-const PopoverHover = () => {
-  const [popoverOpen, setPopoverOpen] = useState(false);
-  const targetRef = useRef(null);
-
-  const togglePopover = () => {
-    setPopoverOpen(!popoverOpen);
-  };
-  return (
-    <div>
-      <Button color='secondary' innerRef={targetRef}>
-        Apertura in Hover
-      </Button>
-
-      <Popover placement='right' trigger='hover' target={targetRef} isOpen={popoverOpen} toggle={togglePopover}>
-        <PopoverHeader>Popover in Hover</PopoverHeader>
-        <PopoverBody>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel finibus augue.</PopoverBody>
-      </Popover>
-    </div>
-  );
-};
-export const _ModalitaHover: Story = PopoverHover.bind({});
-_ModalitaHover.storyName = 'Modalità Hover';
-
-const PopoverFocus = () => {
-  const [popoverOpen, setPopoverOpen] = useState(false);
-  const targetRef = useRef(null);
-
-  const togglePopover = () => {
-    setPopoverOpen(!popoverOpen);
-  };
-  return (
-    <div>
-      <Button tabIndex={0} color='danger' innerRef={targetRef}>
-        Popover richiudibile
-      </Button>
-
-      <Popover placement='right' trigger='focus' target={targetRef} isOpen={popoverOpen} toggle={togglePopover}>
-        <PopoverHeader>Dismissible popover</PopoverHeader>
-        <PopoverBody>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel finibus augue.</PopoverBody>
-      </Popover>
-    </div>
-  );
-};
-export const DismissAlClickSuccessivo: Story = PopoverFocus.bind({});
-DismissAlClickSuccessivo.storyName = 'Dismiss al click successivo';
-
+/* 
 type EsempiInterattiviProps = {
   disabled: boolean;
   placement: 'top' | 'bottom' | 'left' | 'right';
@@ -262,21 +269,23 @@ type EsempiInterattiviProps = {
 };
 
 // using Story or adding types to props breaks storybook
-export const _EsempiInterattivi: Story<EsempiInterattiviProps> = ({ disabled, placement, title, text }) => {
-  const [isOpen, toggle] = useState(false);
-  const ref = useRef(null);
+export const _EsempiInterattivi: Story = {
+  render: ({ disabled, placement, title, text }) => {
+    const [isOpen, toggle] = useState(false);
+    const ref = useRef(null);
 
-  return (
-    <div style={{ padding: 250, textAlign: 'center' }}>
-      <Button innerRef={ref} color='primary' disabled={disabled} onClick={() => toggle(!isOpen)}>
-        Popover {disabled ? 'Disabilitato' : ''}
-      </Button>
-      <Popover placement={placement} target={ref} toggle={() => toggle(!isOpen)} isOpen={isOpen}>
-        <PopoverHeader>{title}</PopoverHeader>
-        <PopoverBody>{text}</PopoverBody>
-      </Popover>
-    </div>
-  );
+    return (
+      <div style={{ padding: 250, textAlign: 'center' }}>
+        <Button innerRef={ref} color='primary' disabled={disabled} onClick={() => toggle(!isOpen)}>
+          Popover {disabled ? 'Disabilitato' : ''}
+        </Button>
+        <Popover placement={placement} target={ref} toggle={() => toggle(!isOpen)} isOpen={isOpen}>
+          <PopoverHeader>{title}</PopoverHeader>
+          <PopoverBody>{text}</PopoverBody>
+        </Popover>
+      </div>
+    );
+  }
 };
 
 _EsempiInterattivi.storyName = 'Esempi interattivi';
@@ -294,4 +303,4 @@ _EsempiInterattivi.argTypes = {
       options: ['top', 'bottom', 'left', 'right']
     }
   }
-};
+}; */
